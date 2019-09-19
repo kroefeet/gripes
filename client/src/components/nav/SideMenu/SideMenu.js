277201805efa34a281bc1d998a9ce80438bd1d2c
Mickle Nguyen <michael.nguyen724@gmail.com>
2019-09-18 18:34:42 -0700

    sidebar movement, testing colors, resized logo and got the title to overlap.

--- client/src/components/nav/SideMenu/SideMenu.js
+++ client/src/components/nav/SideMenu/SideMenu.js
@@ -6,12 +6,23 @@ class SideMenu extends Component {
   render() {
     return (
       <div className="Style">
-          <p>Gripe as you please!</p>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          {/* <p>Gripe as you please!</p> */}
+          <Link to="/">About</Link><br></br>
           <Link to="/gripe/Heavy/">Heavy Gripes</Link><br></br>
           <Link to="/gripe/Light/">Light Gripes</Link><br></br>
-          <Link to="/add-gripe/">Got a gripe?</Link><br></br>
-          <Link to="/">About</Link><br></br>
-        </div>  
+          <Link to="/add-gripe/">Got a Gripe?</Link><br></br>
+        </div>
     )}
 }
 
