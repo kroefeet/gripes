9ff28323004cde7238d4d3617a18bb0e0cdb95ba
Mickle Nguyen <michael.nguyen724@gmail.com>
2019-09-18 20:54:06 -0700

    small background color changes

--- client/src/components/nav/SideMenu/SideMenu.js
+++ client/src/components/nav/SideMenu/SideMenu.js
@@ -19,8 +19,8 @@ class SideMenu extends Component {
           <br></br>
           {/* <p>Gripe as you please!</p> */}
           <Link to="/">About</Link><br></br>
-          <Link to="/gripe/Heavy/">Heavy Gripes</Link><br></br>
-          <Link to="/gripe/Light/">Light Gripes</Link><br></br>
+          <Link to="/gripe/Light/">Light</Link><br></br>
+          <Link to="/gripe/Heavy/">Heavy</Link><br></br>
           <Link to="/add-gripe/">Got a Gripe?</Link><br></br>
         </div>
     )}
