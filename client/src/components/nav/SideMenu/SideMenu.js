b08de505476770d5313592c81f270b013b04b043
Marlyn <mwilliams586@gmail.com>
2019-09-21 15:18:07 -0700

    ...

--- client/src/components/nav/SideMenu/SideMenu.js
+++ client/src/components/nav/SideMenu/SideMenu.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
 
+
 import './SideMenu.css';
 class SideMenu extends Component {
   render() {
     return (
       <div className="Style">
           <p>Gripe as you please!</p>
+          
           <Link to="/gripe/Heavy/">Heavy Gripes</Link><br></br>
           <Link to="/gripe/Light/">Light Gripes</Link><br></br>
           <Link to="/add-gripe/">Got a gripe?</Link><br></br>
